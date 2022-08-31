379fd28569afbbc57147ca8e942daed08bc0e363
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-08-31 21:20:38 +0530

    practice

---
+++ 02.py
@@ -0,0 +1,15 @@
+from cmath import sqrt
+
+if __name__ == '__main__':
+    n = int(input())
+
+
+# for i in range(0,20):
+#     print(i*i)
+#     i=i+1
+#     if i==n:
+#         break
+
+for i in range(0,n):
+    s=i*i
+    print(s)
---
+++ 04.py
@@ -0,0 +1,8 @@
+def number(n):
+    for n in range(1,n+1):
+     print(n,end="")
+    # print(end(""))
+
+
+n = int(input())
+number(n)
