2af24123d313a2a5c20ddf77fb063d52e64a5f15
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-09-03 12:03:16 +0530

    hackerRANK

--- 05.py
+++ 05.py
@@ -1,8 +1,21 @@
-X = int(input())
-Y = int(input())
-Z = int(input())
-N = int(input())
+# X = int(input())
+# Y = int(input())
+# Z = int(input())
+# N = int(input())
 
-ans = [[i, j, k] for i in range(X + 1) for j in range(Y + 1) for k in range(Z + 1) if i + j + k != N]
+# ans = [[i, j] for i in range(X + 1) for j in range(Y + 1) if i + j  != N]
 
-print (ans)
+# print (ans)
+
+from http.client import OK
+
+
+for i in range[3]:
+    print(i)
+    for j in range[4]:
+        print(j)
+        for k in range[5]:
+            print(k)
+
+
+print([i,j,k])
---
+++ 06.py
@@ -0,0 +1,24 @@
+# n = int(input("Enter Number: "))
+
+
+# num = []
+# for i in range(3, 15):
+# 	num.append(i)
+#     # print()
+
+
+
+
+# Create an empty list
+# sample_list = []
+# Iterate over sequence of numbers from 0 to 9
+# for i in range(10):
+    # Append each number at the end of list
+    # sample_list.append(i) 
+    
+# sample_list = [i for i in range(10)]
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split()) 
+    print(sorted(list(set(arr)))[-2])
