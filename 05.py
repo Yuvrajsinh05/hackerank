0de19717039b40d05ffd5dd62824dfbfe4c1b308
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-09-04 19:06:52 +0530

    lP

--- 05.py
+++ 05.py
@@ -1,21 +1,21 @@
-# X = int(input())
-# Y = int(input())
-# Z = int(input())
-# N = int(input())
+X = int(input())
+Y = int(input())
+Z = int(input())
+N = int(input())
 
-# ans = [[i, j] for i in range(X + 1) for j in range(Y + 1) if i + j  != N]
+ans = [[i, j] for i in range(X + 1) for j in range(Y + 1) if i + j  != N]
 
-# print (ans)
+print (ans)
 
-from http.client import OK
+# from http.client import OK
     
 
-for i in range[3]:
-    print(i)
-    for j in range[4]:
-        print(j)
-        for k in range[5]:
-            print(k)
+# for i in range[3]:
+#     print(i)
+#     for j in range[4]:
+#         print(j)
+#         for k in range[5]:
+#             print(k)
 
 
-print([i,j,k])
+# print([i,j,k])
