95865c34cbe37545fd1db06bfb411cc49ee570c6
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-09-04 14:21:34 +0530

    upDate

--- 06.py
+++ 06.py
@@ -18,7 +18,7 @@
     
 # sample_list = [i for i in range(10)]
 
-if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split()) 
-    print(sorted(list(set(arr)))[-2])
+# if __name__ == '__main__':
+n = int(input())
+arr = map(int, input().split()) 
+print(sorted(list(set(arr)))[-2])
---
+++ 07.py
@@ -0,0 +1,18 @@
+# num=int(input("Enter:"))
+# print(num)
+
+
+# List = []
+# name = input("Enter your name:")
+# for i in List:
+#     List.append(name)
+
+
+
+L=[]
+while True:
+  item=input("enter new item :")
+  if item=='':
+    break
+  L.append(int(item))
+print ("List : ",L)
