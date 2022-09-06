2fa10e38bafc200aed6369148198936f0b7f212d
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-09-06 22:26:32 +0530

    Lp

---
+++ 09.py
@@ -0,0 +1,25 @@
+if __name__ == '__main__':
+    N = int(input())
+    L=[];
+    for i in range(0,N):
+        cmd=input().split();
+        if cmd[0] == "insert":
+            L.insert(int(cmd[1]),int(cmd[2]))
+        elif cmd[0] == "append":
+            L.append(int(cmd[1]))
+        elif cmd[0] == "pop":
+            L.pop();
+        elif cmd[0] == "print":
+            print(L)
+        elif cmd[0] == "remove":
+            L.remove(int(cmd[1]))
+        elif cmd[0] == "sort":
+            L.sort();
+        else:
+            L.reverse();
+
+
+# a = input().split()
+# b = [int(a[2]),int(a[3])]
+# print(a)
+# print(b)
