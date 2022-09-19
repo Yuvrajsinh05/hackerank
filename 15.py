e88657745a6cb1c7e1f1d78c4d84de7b0e140694
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-09-19 22:07:27 +0530

    LP

---
+++ 15.py
@@ -0,0 +1,48 @@
+# i = 4
+# d = 4.0
+# s = 'HackerRank '
+# inp=input()
+# # Declare second integer, double, and String variables.
+# sum1 =i+d
+# # Read and save an integer, double, and String to your variables.
+# print(sum1)
+# # Print the sum of both integer variables on a new line.
+# sum2=d+d
+# # Print the sum of the double variables on a new line.
+# print(sum2)
+# # Concatenate and print the String variables on a new line
+# # The 's' variable above should be printed first.
+# print(s+inp)
+
+# # Declare second integer, double, and String variables.
+# inp1=int(input("enter num1"))
+# inp2=int(input("enter num2"))
+# inp3=input("enter text")
+# # Read and save an integer, double, and String to your variables.
+# inp1 +=i
+# inp2 +=d
+# # Print the sum of both integer variables on a new line.
+# print(inp1)
+# # Print the sum of the double variables on a new line.
+# print(inp2)
+# # Concatenate and print the String variables on a new line
+# # The 's' variable above should be printed first.
+# print(s+inp3)
+
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+j=int(input())
+# Read and save an integer, double, and String to your variables.
+e=float(input())
+# Print the sum of both integer variables on a new line.
+t=(input())
+print (i+j)
+# Print the sum of the double variables on a new line.
+print (d+e) 
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print (s+t)
