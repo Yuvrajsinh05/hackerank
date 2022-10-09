64ab9b4407f55992053e429f989a63a80b421767
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-10-09 22:48:59 +0530

    python

--- 04.py
+++ 04.py
@@ -1,11 +1,11 @@
-n = int(input())
-def number(n):
-    for n in range(1,n+1):
-     print(n,end="")
-    # print(end(""))
-
-number(n)
 # n = int(input())
-# # number(n)
-# a = [x for x in range(1,n+1)]
-# print(a)
+# def number(n):
+#     for n in range(1,n+1):
+#      print(n,end="")
+#     # print(end(""))
+
+# number(n)
+n = int(input())
+# number(n)
+a = [x for x in range(1,n+1)]
+print(a)
